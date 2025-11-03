0c42b0f912fb6a209c89b0f357ce5a739881a52a
Shuta Suzuki <shutas@umich.edu>
2025-11-03 23:55:19 +0900

    MOD: Finished files for "Loops"

--- Javascript/Loops/main.js
+++ Javascript/Loops/main.js
@@ -89,3 +89,34 @@ for (let i = 10; i >= 0; i--) {
   }
   output.appendChild(para);
 }
+
+// Filling in a guest list
+const people = [
+  "Chris",
+  "Anne",
+  "Colin",
+  "Terri",
+  "Phil",
+  "Lola",
+  "Sam",
+  "Kay",
+  "Bruce",
+];
+
+const admitted = document.querySelector(".admitted");
+const refused = document.querySelector(".refused");
+admitted.textContent = "Admit: ";
+refused.textContent = "Refuse: ";
+
+// loop starts here
+for (const person of people) {
+  if (person === "Phil" || person === "Lola") {
+    refused.textContent += person + ", ";
+  } else {
+    admitted.textContent += person + ", ";
+  }
+}
+
+// delete extra period and space
+refused.textContent = refused.textContent.slice(0,refused.textContent.length-2);
+admitted.textContent = admitted.textContent.slice(0,admitted.textContent.length-2);
