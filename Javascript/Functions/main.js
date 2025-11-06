80ed92077f3c281d39f90f6b5d30ca1297090d4f
Shuta Suzuki <shutas@umich.edu>
2025-11-06 10:14:38 +0900

    MOD: Finished files for "Functions"

--- Javascript/Functions/main.js
+++ Javascript/Functions/main.js
@@ -18,3 +18,22 @@ function hello(name = "Chris") {
 
 hello("Ari");
 hello();
+
+const x = 1;
+
+function a() {
+    const y = 2;
+    output(y);
+}
+
+function b() {
+    const z = 3;
+    output(z);
+}
+
+function output(value) {
+    console.log(`Value: ${value}`);
+}
+
+a();
+b();
