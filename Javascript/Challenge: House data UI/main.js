29127b7508e8900e69accad7ee68b8463cb92132
Shuta Suzuki <shutas@umich.edu>
2025-12-16 11:09:07 +0900

    MOD: Final files for "Challenge: House data UI"

--- Javascript/Challenge: House data UI/main.js
+++ Javascript/Challenge: House data UI/main.js
@@ -23,8 +23,11 @@ function fetchHouseData() {
 }
 
 function initializeForm() {
-  // Initialize choose-street
+
   let collectedStreets = [];
+  let currentLargestBedroomNum = 0;
+  let currentLargestBathroomNum = 0;
+  
   for (const house of houses) {
     if (collectedStreets.indexOf(house.street) === -1) {
       collectedStreets.push(house.street);
@@ -33,11 +36,27 @@ function initializeForm() {
       newOption.textContent = house.street;
       streetSelect.appendChild(newOption);
     }
+    if (currentLargestBathroomNum < house.bathrooms) {
+      currentLargestBathroomNum = house.bathrooms;
+    }
+    if (currentLargestBedroomNum < house.bedrooms) {
+      currentLargestBedroomNum = house.bedrooms;
+    }
   }
 
-  // Initialize choose-bedrooms
+  for (let i = 1; i <= currentLargestBedroomNum; i++) {
+    let newOption = document.createElement("option");
+    newOption.value = i;
+    newOption.textContent = i;
+    bedroomSelect.appendChild(newOption);
+  }
 
-  // Initialize choose-bathrooms
+  for (let i = 1; i <= currentLargestBathroomNum; i++) {
+    let newOption = document.createElement("option");
+    newOption.value = i;
+    newOption.textContent = i;
+    bathroomSelect.appendChild(newOption);
+  }
 }
 
 function renderHouses(e) {
@@ -45,6 +64,44 @@ function renderHouses(e) {
   e.preventDefault();
 
   // Add rest of code here
+  let filteredHouse = houses.filter((house) => {
+    return (streetSelect.value === String(house.street) || streetSelect.value === "") && (bathroomSelect.value === String(house.bathrooms) || bathroomSelect.value === "") && (bedroomSelect.value === String(house.bedrooms) || bedroomSelect.value === "");
+  });
+
+  resultCount.textContent = `Results returned: ${filteredHouse.length}`;
+
+  output.replaceChildren();
+
+  function renderHouse(house) {
+    let totalArea = 0;
+    for (const room in house.room_sizes) {
+      totalArea += house.room_sizes[room];
+    }
+    let newArticle = document.createElement("article");
+    let newH2 = document.createElement("h2");
+    let newUL = document.createElement("ul");
+    let newBedrooms = document.createElement("li");
+    let newBathrooms = document.createElement("li");
+    let newRoomArea = document.createElement("li");
+    let newPrice = document.createElement("li");
+    newH2.textContent = `${house.house_number} ${house.street}`;
+    newBedrooms.textContent = `🛏️ Bedrooms: ${house.bedrooms}`;
+    newBathrooms.textContent = `🛀 Bathrooms: ${house.bathrooms}`;
+    newRoomArea.textContent = `Room area: ${totalArea} m²`;
+    newPrice.textContent = `Price: £${house.price}`;
+    newUL.appendChild(newBedrooms);
+    newUL.appendChild(newBathrooms);
+    newUL.appendChild(newRoomArea);
+    newUL.appendChild(newPrice);
+    newArticle.appendChild(newH2);
+    newArticle.appendChild(newUL);
+    output.appendChild(newArticle);
+  }
+
+  for (const house of filteredHouse) {
+    renderHouse(house);
+  }
+
 }
 
 // Add a submit listener to the <form> element
