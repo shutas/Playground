79365c16e101763a6c3518894e58c30957d3d628
Shuta Suzuki <shutas@umich.edu>
2026-01-08 21:53:22 +0900

    MOD: Added delete functionality

--- React/moz-todo-react/src/App.jsx
+++ React/moz-todo-react/src/App.jsx
@@ -7,12 +7,29 @@ import Todo from "./components/Todo";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
   const taskList = tasks?.map((task) => (
     <Todo 
       id={task.id}
       name={task.name}
       completed={task.completed} 
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
 
@@ -20,6 +37,10 @@ function App(props) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false};
     setTasks([...tasks, newTask]);
   }
+
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -30,7 +51,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
