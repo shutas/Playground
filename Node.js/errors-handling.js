6ac52037271f640e304c7fbed1b10ee72473a40f
Shuta Suzuki <shutas@umich.edu>
2026-02-03 09:38:36 +0900

    MOD: Final files for "Node Errors Handling"

--- Node.js/errors-handling.js
+++ Node.js/errors-handling.js
@@ -1,4 +1,4 @@
-/*const fs = require("fs");
+const fs = require("fs");
 fs.readFile("nonexistent.txt", (err) => {
     console.error(err.code);
 });
@@ -36,7 +36,6 @@ readConfigFile("config.json", (err, config) => {
     }
     console.log("Config loaded successfully:", config);
 });
-*/
 
 const fs = require("fs").promises;
 
@@ -70,3 +69,53 @@ async function loadUserData(userId) {
         console.error("Failed to load user data:", error.message);
     }
 })();
+
+process.on("uncaughtException", (error) => {
+    console.error("UNCAUGHT EXCEPTION! Shutting down...");
+    console.error(error.name, error.message);
+
+    server.close(() => {
+        console.log("Process terminated due to uncaught exception");
+        process.exit(1);
+    });
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+    console.error("UNHANDLED REJECTION! Shutting down...");
+    console.error("Unhandled Rejection at:", promise, "Reason:", reason);
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+Promise.reject(new Error("Something went wrong"));
+
+setTimeout(() => {
+    throw new Error("Uncaught exception after timeout");
+},1000);
+
+class ValidationError extends Error {
+    constructor(message, field) {
+        super(message);
+        this.name = "ValidationError";
+        this.field = field;
+        this.statusCode = 400;
+    }
+}
+
+class NotFoundError extends Error {
+    constructor(resource) {
+        super(`${resource} not found`);
+        this.name = "NotFoundError";
+        this.statusCode = 404;
+    }
+}
+
+function getUser(id) {
+    if(!id) {
+        throw new ValidationError("User ID is required", "id");
+    }
+}
+
+getUser(0);
