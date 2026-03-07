d72223207919cd9211e31e993c867c1b9a3fec67
Shuta Suzuki <shutas@umich.edu>
2026-03-07 23:35:43 +0900

    ADD: Final files for "Update"

---
+++ MySQL/update-table.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+let mysql = require("mysql2");
+
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  let sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+});
