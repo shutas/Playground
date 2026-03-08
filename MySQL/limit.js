4082e788892714cd119d383feb2088ffaeba4272
Shuta Suzuki <shutas@umich.edu>
2026-03-08 17:31:33 +0900

    ADD: Final files for "Join"

---
+++ MySQL/limit.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+let mysql = require("mysql2");
+
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  let sql = "SELECT * FROM customers LIMIT 5 OFFSET 2";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
